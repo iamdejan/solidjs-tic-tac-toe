1807359541f73aa2deb848022059e908b6495307
Giovanni Dejan <15686093+iamdejan@users.noreply.github.com>
2025-06-29 22:57:22 +0700

    feat: leave room (#3)

--- src/pages/WaitingRoom.tsx
+++ src/pages/WaitingRoom.tsx
@@ -1,7 +1,7 @@
 import { JSX } from "solid-js/jsx-runtime";
 import useRoomID from "../hooks/useRoomID";
 import { useWebSocket } from "solidjs-use";
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, Show } from "solid-js";
 import WebSocketResponse from "../types/CreateRoomResponse";
 import useUserID from "../hooks/useUserID";
 import useCharacter from "../hooks/useCharacter";
@@ -9,9 +9,13 @@ import WSMessage from "../types/WSMessage";
 import { useNavigate } from "@solidjs/router";
 
 export default function WaitingRoom(): JSX.Element {
-  const roomID = useRoomID((state) => state.roomID);
   const userID = useUserID((state) => state.userID);
+
+  const roomID = useRoomID((state) => state.roomID);
+  const unsetRoomID = useRoomID((state) => state.unsetRoomID);
+
   const setCharacter = useCharacter((state) => state.setCharacter);
+
   const { status, send, data } = useWebSocket<string>(
     "wss://localhost:8080/ws",
   );
@@ -55,6 +59,10 @@ export default function WaitingRoom(): JSX.Element {
 
         setCharacter(parsedResponse.character);
         break;
+      case "ROOM_LEFT":
+        unsetRoomID();
+        navigate("/");
+        break;
       case "GAME_STARTED":
         navigate("/game-room");
         break;
@@ -63,12 +71,37 @@ export default function WaitingRoom(): JSX.Element {
     }
   });
 
+  const [isLeaveInProcess, setIsLeaveInProcess] = createSignal<boolean>(false);
+
+  function leaveRoom() {
+    const message: WSMessage = {
+      command: "leave",
+      params: {
+        room_id: roomID(),
+        user_id: userID(),
+      },
+    };
+    send(JSON.stringify(message));
+    setIsLeaveInProcess(true);
+  }
+
   return (
     <div class="flex flex-col justify-center items-center h-[100vh] w-[100vw] gap-8">
       <h2 class="text-2xl">Room {roomID()}</h2>
       <p>
         Waiting for other player... <span class="loading loading-spinner" />
       </p>
+      <button
+        type="button"
+        class="btn"
+        onClick={leaveRoom}
+        disabled={isLeaveInProcess()}
+      >
+        Leave
+        <Show when={isLeaveInProcess()}>
+          <span class="loading loading-spinner" />
+        </Show>
+      </button>
     </div>
   );
 }
